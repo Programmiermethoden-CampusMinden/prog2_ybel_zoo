ac56dfafd0176f6c6e5d92f62cda3fa558d67a97
Carsten Gips <cagix@hsbi.de>
2024-03-08 16:10:50 +0100

    add files for zoo task
    add link to task (readme)

---
+++ src/main/java/zoo/mammals/cats/Cat.java
@@ -0,0 +1,6 @@
+package zoo.mammals.cats;
+
+import zoo.mammals.Mammal;
+
+/** The cat family belongs to the class of mammals. */
+public interface Cat extends Mammal {}
---
+++ src/main/java/zoo/mammals/rodents/Rodent.java
@@ -0,0 +1,6 @@
+package zoo.mammals.rodents;
+
+import zoo.mammals.Mammal;
+
+/** The order of rodents belongs to the class of mammals. */
+public interface Rodent extends Mammal {}
